24cbe46fb96d43729e6f489b32b513b830e3a168
Matt Fowles <matt.fowles@gmail.com>
2012-12-12 21:26:26 -0800

    Remove the redundant onReady

--- crossword.js
+++ crossword.js
@@ -6,8 +6,7 @@ if (Meteor.isServer) {
 
 if (Meteor.isClient) {
   Meteor.startup( function () {
-    $(document).ready( function () {
-      var samplePuzzle = { "row" : [
+    var samplePuzzle = { "row" : [
       "STUMP_TERM_LIMA",
       "TEPID_OLEO_ORAL",
       "INSTALLSFORTIME",
@@ -23,10 +22,10 @@ if (Meteor.isClient) {
       "INJURYSELECTION",
       "FEAR_DODO_ATONE",
       "TERN_STIR_TONYS"
-      ] };
+    ] };
 
-      var sampleClues =  { 
-        "across" : [
+    var sampleClues =  { 
+      "across" : [
         { no : "1",  text : "Baffle" },
         { no : "6",  text : "Section of a school year" },
         { no : "10",  text : "Latin American capital" },
@@ -63,65 +62,63 @@ if (Meteor.isClient) {
         { no : "64",  text : "Make amends" },
         { no : "65",  text : "Bird on a beach" },
         { no : "66",  text : "Cookbook instruction" }
-        ], 
-          "down" : [
-          { no : "1",  text : "Clown's walking poles" },
-          { no : "2",  text : "Many a faculty member's goal" },
-          { no : "3",  text : "Honorable" },
-          { no : "4",  text : "Gaynor from 'South Pacific'" },
-          { no : "5",  text : "Mobile electronic gadget" },
-          { no : "6",  text : "Ancient Mexican" },
-          { no : "7",  text : "Or ___ ( bullying words )" },
-          { no : "8",  text : "TD caller" },
-          { no : "9",  text : "Guernsey's greeting" },
-          { no : "10",  text : "A blast" },
-          { no : "11",  text : "Fleur-de-lis flower" },
-          { no : "12",  text : "Patrick's auntie" },
-          { no : "13",  text : "Yankee slugger Rodriguez" },
-          { no : "18",  text : "Property claims by creditors" },
-          { no : "19",  text : "Foreclosed property, briefly" },
-          { no : "24",  text : "Causes of some head scratching" },
-          { no : "25",  text : "Certain U.S. agent" },
-          { no : "26",  text : "Yearning" },
-          { no : "29",  text : "European cars with a lightning bolt logo" },
-          { no : "30",  text : "Takeover of human activity by machines" },
-          { no : "31",  text : "'My Goodness!'" },
-          { no : "32",  text : "Some rapid transits" },
-          { no : "34",  text : "At an improper time" },
-          { no : "37",  text : "__ Beta Kappa" },
-          { no : "38",  text : "Take off" },
-          { no : "39",  text : "Mt Olympus ruler" },
-          { no : "40",  text : "'___ and the Swan' ( Yeats )" },
-          { no : "42",  text : "Die down" },
-          { no : "43",  text : "Mountaineer's resting place" },
-          { no : "45",  text : "Stratum" },
-          { no : "46",  text : "Person with a fitting career?" },
-          { no : "48",  text : "Roman general in a Shakespeare title" },
-          { no : "49",  text : "Jewelry gems" },
-          { no : "52",  text : "'Be prepared', e.g." },
-          { no : "53",  text : "Make fine, in a way" }, 
-          { no : "54",  text : "Where the femur and the tibia meet" },
-          { no : "55",  text : "Clearly not locked" },
-          { no : "56",  text : "'Star Wars' elite" },
-          { no : "59",  text : "Upholsterer's meas." },
-          { no : "60",  text : "Lush" },
-          { no : "61",  text : "Alley frequenter" }
-        ]
-      };
+      ], 
+      "down" : [
+        { no : "1",  text : "Clown's walking poles" },
+        { no : "2",  text : "Many a faculty member's goal" },
+        { no : "3",  text : "Honorable" },
+        { no : "4",  text : "Gaynor from 'South Pacific'" },
+        { no : "5",  text : "Mobile electronic gadget" },
+        { no : "6",  text : "Ancient Mexican" },
+        { no : "7",  text : "Or ___ ( bullying words )" },
+        { no : "8",  text : "TD caller" },
+        { no : "9",  text : "Guernsey's greeting" },
+        { no : "10",  text : "A blast" },
+        { no : "11",  text : "Fleur-de-lis flower" },
+        { no : "12",  text : "Patrick's auntie" },
+        { no : "13",  text : "Yankee slugger Rodriguez" },
+        { no : "18",  text : "Property claims by creditors" },
+        { no : "19",  text : "Foreclosed property, briefly" },
+        { no : "24",  text : "Causes of some head scratching" },
+        { no : "25",  text : "Certain U.S. agent" },
+        { no : "26",  text : "Yearning" },
+        { no : "29",  text : "European cars with a lightning bolt logo" },
+        { no : "30",  text : "Takeover of human activity by machines" },
+        { no : "31",  text : "'My Goodness!'" },
+        { no : "32",  text : "Some rapid transits" },
+        { no : "34",  text : "At an improper time" },
+        { no : "37",  text : "__ Beta Kappa" },
+        { no : "38",  text : "Take off" },
+        { no : "39",  text : "Mt Olympus ruler" },
+        { no : "40",  text : "'___ and the Swan' ( Yeats )" },
+        { no : "42",  text : "Die down" },
+        { no : "43",  text : "Mountaineer's resting place" },
+        { no : "45",  text : "Stratum" },
+        { no : "46",  text : "Person with a fitting career?" },
+        { no : "48",  text : "Roman general in a Shakespeare title" },
+        { no : "49",  text : "Jewelry gems" },
+        { no : "52",  text : "'Be prepared', e.g." },
+        { no : "53",  text : "Make fine, in a way" }, 
+        { no : "54",  text : "Where the femur and the tibia meet" },
+        { no : "55",  text : "Clearly not locked" },
+        { no : "56",  text : "'Star Wars' elite" },
+        { no : "59",  text : "Upholsterer's meas." },
+        { no : "60",  text : "Lush" },
+        { no : "61",  text : "Alley frequenter" }
+      ]
+    };
 
-      $(this).crossword(
-          { 
-          'puzzle' 			: samplePuzzle, //JSON puzzle structure  				
-          'clues' 			: sampleClues, //JSON clue structure
-          'title'				: "In the Beginning by Lynn Lempel", //A title for the puzzle
-          'puzzleContainer'	: "#puzzleContainer", // id of puzzle display DIV 
-          'acrossContainer'	: "#acrossContainer", // id of across clues display DIV
-          'downContainer'		: "#downContainer", // id of down clues display DIV
-          'revealButton'		: "#revealButton", // id of reveal button
-          'hideButton'		: "#hideButton", // id of hide button
-          'saveButton'		: "#saveButton",  // id of save to cookie button
-          'loadButton'		: "#loadButton"  // id of load from cookie  button
-          } );
-      });
+    $(this).crossword( { 
+        'puzzle'            : samplePuzzle, //JSON puzzle structure                  
+        'clues'             : sampleClues, //JSON clue structure
+        'title'             : "In the Beginning by Lynn Lempel", //A title for the puzzle
+        'puzzleContainer'   : "#puzzleContainer", // id of puzzle display DIV 
+        'acrossContainer'   : "#acrossContainer", // id of across clues display DIV
+        'downContainer'     : "#downContainer", // id of down clues display DIV
+        'revealButton'      : "#revealButton", // id of reveal button
+        'hideButton'        : "#hideButton", // id of hide button
+        'saveButton'        : "#saveButton",  // id of save to cookie button
+        'loadButton'        : "#loadButton"  // id of load from cookie  button
+        } );
   });
 }
