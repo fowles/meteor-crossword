00f6403d99b3e821e89b960e20cb1a108f732792
Matt Fowles <matt.fowles@gmail.com>
2012-12-16 16:20:14 -0800

    Build the board dynamically

--- crossword.js
+++ crossword.js
@@ -1,9 +1,24 @@
+function Element(val) {
+    this.value = val;
+}
+
+function Board(w, h) {
+  this.elems = []
+  for(var i = 0; i < w; ++i) {
+    var r = []
+    for(var j = 0; j < h; ++j) {
+        r.push(new Element(i*j))
+    }
+    this.elems.push(r)
+  }
+}
+
+Boards = new Meteor.Collection("boards")
+Boards.insert(new Board(3,3))
+
 if (Meteor.isClient) {
   Template.board.elems = function() {
-    return [
-        [{'value':1}, {'value':2}],
-        [{'value':3}, {'value':4}]
-    ];
+    return Boards.findOne().elems;
   }
 }
 
