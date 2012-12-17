c9ae14ed8c47f57193992a574bef29944d99b2ba
Matt Fowles <matt.fowles@gmail.com>
2012-12-16 22:59:32 -0800

    Add name, width, and height parameters

--- crossword.js
+++ crossword.js
@@ -1,28 +1,48 @@
 Boards = new Meteor.Collection("boards")
 
 function Element(val, board) {
-    this.value = val;
+  this.value = val;
 }
 
-function Board(w, h) {
-  this.elems = []
+function setBoardDimensions(b, w, h) {
+  var old = b.elems || []
+  b.elems = []
+  b.width = w
+  b.height = h
   for(var i = 0; i < w; ++i) {
     var r = []
     for(var j = 0; j < h; ++j) {
-        r.push(new Element(i*j))
+      r.push((old[i] || [])[j] || new Element(i*j))
     }
-    this.elems.push(r)
+    b.elems.push(r)
   }
+}
+
+
+function Board(w, h) {
+  this.name = undefined
+  this.foo = 12
+  setBoardDimensions(this, w, h)
 }
 
 
 if (Meteor.isClient) {
+  Handlebars.registerHelper("name", function() {
+    return this.name || this._id
+  })
+
+  var updateBoard = function (b) {
+    Boards.update(b._id, b)
+    Session.set("active-board", b)
+  }
+
   Template.controls.boards = function() {
     return Boards.find()
   }
+
   Template.controls.events({
     'click .board' : function (evt) {
-      Session.set('active-board', this)
+      updateBoard(this)
     }
   })
 
@@ -33,12 +53,21 @@ if (Meteor.isClient) {
   Template.board.events({
     'click .cell': function (evt) {
       this.value += 1
-      return true
     },
     'click .board': function (evt) {
-      Boards.update(this._id, this)
-      Session.set("active-board", this)
-      return true
+      updateBoard(this)
+    },
+    'keyup #name': function(evt) {
+      this.name = evt.target.value
+      updateBoard(this)
+    },
+    'change #width': function(evt) {
+      setBoardDimensions(this, evt.target.value, this.height)
+      updateBoard(this)
+    },
+    'change #height': function(evt) {
+      setBoardDimensions(this, this.width, evt.target.value)
+      updateBoard(this)
     }
   })
 }
