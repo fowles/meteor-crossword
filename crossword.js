33bd74f70e843e230d4f02c56aba4ef6d2c2da45
Matt Fowles <matt.fowles@gmail.com>
2012-12-16 19:35:13 -0800

    Add a history of boards to choose from

--- crossword.js
+++ crossword.js
@@ -28,20 +28,29 @@ function Board(w, h) {
 
 
 if (Meteor.isClient) {
+  Template.controls.boards = function() {
+    return Boards.find()
+  }
+  Template.controls.events({
+    'click .board' : function (evt) {
+      Session.set('active-board', this)
+    }
+  })
+
   Template.board.b = function() {
-    return Boards.findOne();
+    return Session.get("active-board") || Boards.findOne()
   }
 
   Template.board.events({
     'click .cell': function (evt) {
       this.value += 1
-      return true;
+      return true
     },
     'click .board': function (evt) {
-      Boards.update(this._id, this)
-      return true;
+      Session.set("active-board", this)
+      return true
     }
-  });
+  })
 }
 
 if (Meteor.isServer) {
